3781d65f91ce114a82e35d7d82bdc8f1887ae3d0
KOR Jean-pierre <tersookor758@gmail.com>
2026-08-12 09:47:59 +0100

    I edited the extensions file

--- extensions.py
+++ extensions.py
@@ -7,6 +7,7 @@ circular imports across the application.
 
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 
 # ==========================================================
@@ -29,6 +30,13 @@ login_manager.login_message = (
 )
 
 login_manager.login_message_category = "warning"
+
+
+# ==========================================================
+# JWT Manager
+# ==========================================================
+
+jwt = JWTManager()
 
 
 # ==========================================================
