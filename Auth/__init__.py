3a3e06ee2abf5effb1083b861d7de0e9a6de2a77
KOR Jean-pierre <tersookor758@gmail.com>
2026-08-17 13:58:09 +0100

    I made changes

--- Auth/__init__.py
+++ Auth/__init__.py
@@ -13,5 +13,12 @@ from flask import Blueprint
 auth_bp = Blueprint(
     "auth",
     __name__,
-    template_folder="../templates"
+    template_folder="../templates",
 )
+
+
+# Import authentication routes after creating the blueprint.
+#
+# This is required so that the @auth_bp.route(...) decorators
+# in auth/routes.py are executed and the routes are registered.
+from . import routes
